c79a6ced3a103714322a2d269c2c9eaf5a8f67bf
pedrokorwa <korwapedro@gmail.com>
2021-02-21 23:21:15 +0700

    Tambahan

---
+++ Dev/kamu.java
@@ -0,0 +1,5 @@
+public class kamu {
+    public static void main(String[] args) {
+        System.out.println("bajarlah dengan penuh semangat ");
+    }
+}
---
+++ Dev/sa.java
@@ -0,0 +1,6 @@
+public class sa {
+    public static void main(String[] args) {
+        System.out.println("ayo belajar java bersama saya di sini");
+    }
+
+}
