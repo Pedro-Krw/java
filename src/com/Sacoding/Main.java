fae87a92208204dbd1ea627f0436ecd9e835080d
Pedro'Korwa <59316805+Pedro-korwa@users.noreply.github.com>
2021-02-28 13:17:46 +0700

    13:17

--- src/com/Sacoding/Main.java
+++ src/com/Sacoding/Main.java
@@ -1,134 +1,17 @@
-package com.Sacoding;
-
-public class Main {
-public static void main( String[] args) {
-	
-	// Tipe data dalam bahasa Java
-	// integer, byte, short, long, double, float, boolean
-	
-	// INTEGER (Satuan)
-	
-	int i = 10;
-	
-	System.out.println("=====INTEGER=====");
-	System.out.println("nialai integer i = " + (i + 1));
-	System.out.println("Nilai Max = " + Integer.MAX_VALUE);
-	System.out.println("Nilai Min = " + Integer.MIN_VALUE);
-    System.out.println("Besar integer " + Integer.BYTES + " bytes");
-    System.out.println("Besar integer " + Integer.SIZE + " bit");
-	
-	
-	
-	// byte(Satuan)
-	
-	int b = 10;
-	
-	System.out.println("=====BYTE=====");
-	System.out.println("nialai byte  = " + b);
-	System.out.println("Nilai Max = " + Byte.MAX_VALUE);
-	System.out.println("Nilai Min = " + Byte.MIN_VALUE);
-    System.out.println("Besar byte " + Byte.BYTES + " bytes");
-    System.out.println("Besar byte " + Byte.SIZE + " bit");
-	
-    
-    
-	// short (Satuan)
-	
-	int s = 10;
-	
-	System.out.println("=====SHORT=====");
-	System.out.println("nialai byte  = " + s);
-	System.out.println("Nilai Max = " + Short.MAX_VALUE);
-	System.out.println("Nilai Min = " + Short.MIN_VALUE);
-    System.out.println("Besar byte " + Short.BYTES + " bytes");
-    System.out.println("Besar byte " + Short.SIZE + " bit");
-	
-    
-	// long (Satuan)
-	
-	int l = 10;
-	
-	System.out.println("=====LONG=====");
-	System.out.println("nialai byte i = " + l);
-	System.out.println("Nilai Max = " + Long.MAX_VALUE);
-	System.out.println("Nilai Min = " + Long.MIN_VALUE);
-    System.out.println("Besar byte " + Long.BYTES + " bytes");
-    System.out.println("Besar byte " + Long.SIZE + " bit");
-	
-    
-	// double (Koma Bilangan Real)
-	
-	int d = 10;
-	
-	System.out.println("=====DOUBLE=====");
-	System.out.println("nialai byte i = " + d);
-	System.out.println("Nilai Max = " + Double.MAX_VALUE);
-	System.out.println("Nilai Min = " + Double.MIN_VALUE);
-    System.out.println("Besar byte " + Double.BYTES + " bytes");
-    System.out.println("Besar byte " + Double.SIZE + " bit");
-	
-    
-    
-	// float (Koma Bilangan Real)
-	
-	int f = 10;
-	
-	System.out.println("=====FLOAT=====");
-	System.out.println("nialai byte i = " + f);
-	System.out.println("Nilai Max = " + Float.MAX_VALUE);
-	System.out.println("Nilai Min = " + Float.MIN_VALUE);
-    System.out.println("Besar byte " + Float.BYTES + " bytes");
-    System.out.println("Besar byte " + Float.SIZE + " bit");
-	
-    
-	// char (Koma Bilangan Real Berdasarkan ASCII)
-	
-	int c = 10;
-	
-	System.out.println("=====CHAR=====");
-	System.out.println("nialai byte i = " + c);
-	System.out.println("Nilai Max = " + Character.MAX_VALUE);
-	System.out.println("Nilai Min = " + Character.MIN_VALUE);
-    System.out.println("Besar byte " + Character.BYTES + " bytes");
-    System.out.println("Besar byte " + Character.SIZE + " bit");
-	
-    
-	// boolean (Nialai true atau false)
-	
-	boolean val = false;
-	
-	System.out.println("=====BOOLEAN=====");
-	System.out.println("nialai byte val = " + val);
-	System.out.println("Nilai Max = " + Boolean.TRUE);
-	System.out.println("Nilai Min = " + Boolean.FALSE);
-  
-	
-    
-
-    
-    
-    
-}
-	
-	
-	
-	
-}
-
-
 <?xml version="1.0" encoding="utf-8"?>
 <LinearLayout xmlns:android="http://schemas.android.com/apk/res/android"
     android:orientation="vertical"
     xmlns:app="http://schemas.android.com/apk/res-auto"
     xmlns:tools="http://schemas.android.com/tools"
     android:layout_width="match_parent"
+    android:background="#EFEFEF"
     android:layout_height="match_parent"
     tools:context=".MainActivity">
-
+<!--Bagian Navigasi bar-->
     <androidx.appcompat.widget.Toolbar
-          android:background="@color/purple_200"
-          android:layout_width="match_parent"
-          android:layout_height="wrap_content">
+        android:background="@color/purple_200"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content">
 
         <LinearLayout
             android:orientation="horizontal"
@@ -138,9 +21,9 @@ public static void main( String[] args) {
             <TextView
                 android:layout_gravity="center"
                 android:textSize="19dp"
-                android:textColor="@color/black"
+                android:textColor="@color/white"
                 android:textStyle="bold"
-                android:text="Biak Online Store"
+                android:text="Biak Ngemil"
                 android:layout_width="wrap_content"
                 android:layout_height="wrap_content"/>
 
@@ -149,23 +32,205 @@ public static void main( String[] args) {
                 android:layout_height="match_parent"/>
 
             <Button
-                android:layout_width="209dp"
+                android:layout_weight="1"
+                android:layout_width="265dp"
                 android:layout_height="wrap_content"
-                android:layout_gravity="center"
+                android:gravity="center_vertical"
                 android:backgroundTint="@color/white"
-                android:textColor="@color/black"
+                android:drawableLeft="@drawable/ic_cari"
                 android:text="Cari Sesuatu"
                 android:textAllCaps="false"
-                android:drawableStart="@drawable/ic_cari"/>
+                android:textColor="@color/black" />
+            <androidx.cardview.widget.CardView
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"/>
 
-            <Button
-                android:layout_width="17dp"
-                android:layout_height="match_parent"
-                android:background="@drawable/ic_favorit" />
 
 
         </LinearLayout>
 
     </androidx.appcompat.widget.Toolbar>
+
+<!--  Ini bagian Header  -->
+<LinearLayout
+    android:background="@drawable/header"
+    android:layout_width="match_parent"
+    android:layout_height="120dp"/>
+
+<!--Splash-->
+    <LinearLayout
+        android:padding="8dp"
+        android:orientation="horizontal"
+        android:layout_marginLeft="12dp"
+        android:layout_marginRight="12dp"
+        android:layout_marginTop="-115dp"
+        android:background="@drawable/splas"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content">
+
+        <LinearLayout
+            android:gravity="center"
+            android:layout_gravity="center"
+            android:layout_width="wrap_content"
+            android:layout_height="121dp"
+            android:orientation="vertical">
+
+            <androidx.cardview.widget.CardView
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                app:cardCornerRadius="20dp">
+
+                <ImageView
+                    android:layout_width="160dp"
+                    android:layout_height="164dp"
+                    android:layout_marginLeft="-7dp"
+                    android:layout_marginTop="-20dp"
+                    android:adjustViewBounds="true"
+                    android:cropToPadding="true"
+                    android:src="@drawable/waffel" />
+            </androidx.cardview.widget.CardView>
+        </LinearLayout>
+
+        <LinearLayout
+            android:padding="5dp"
+            android:orientation="vertical"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content">
+            <TextView
+                android:textColor="@color/black"
+                android:textStyle="bold"
+                android:layout_marginLeft="20dp"
+                android:text="Khusus Hari Ini"
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"/>
+            <TextView
+                android:textColor="@color/black"
+                android:textStyle="bold"
+                android:textSize="20dp"
+                android:layout_marginLeft="20dp"
+                android:text="Paket Komplit Waffel"
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"/>
+            <TextView
+                android:textColor="@color/black"
+                android:textStyle="bold"
+                android:textSize="12dp"
+                android:layout_marginLeft="20dp"
+                android:text="Rp 15.000"
+                android:layout_width="match_parent"
+                android:layout_height="wrap_content"/>
+
+            <Button
+                android:layout_width="wrap_content"
+                android:layout_height="wrap_content"
+                android:backgroundTint="#03506f"
+                android:text="Ambil Promo"
+                android:textAllCaps="false"
+                android:textColor="@color/white" />
+
+        </LinearLayout>
+    </LinearLayout>
+
+<LinearLayout
+    android:padding="15dp"
+    android:orientation="horizontal"
+    android:layout_width="match_parent"
+    android:layout_height="wrap_content">
+<!--Card menu-->
+    <LinearLayout
+        android:layout_weight="1"
+        android:foreground="?android:attr/selectableItemBackground"
+        android:clickable="true"
+        android:background="@drawable/card_button"
+        android:padding="20dp"
+        android:orientation="horizontal"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content">
+        <ImageView
+            android:adjustViewBounds="true"
+            android:cropToPadding="true"
+            android:src="@drawable/ic_add_event"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"/>
+
+    </LinearLayout>
+    <RelativeLayout
+        android:layout_width="40dp"
+        android:layout_height="40dp"/>
+    <!--Card menu-->
+    <LinearLayout
+        android:layout_weight="1"
+        android:foreground="?android:attr/selectableItemBackground"
+        android:clickable="true"
+        android:background="@drawable/card_button"
+        android:padding="20dp"
+        android:orientation="horizontal"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content">
+        <ImageView
+            android:adjustViewBounds="true"
+            android:cropToPadding="true"
+            android:src="@drawable/ic_add_event"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"/>
+
+    </LinearLayout>
+    <RelativeLayout
+        android:layout_width="40dp"
+        android:layout_height="40dp"/>
+    <!--Card menu-->
+    <LinearLayout
+        android:layout_weight="1"
+        android:foreground="?android:attr/selectableItemBackground"
+        android:clickable="true"
+        android:background="@drawable/card_button"
+        android:padding="20dp"
+        android:orientation="horizontal"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content">
+        <ImageView
+            android:adjustViewBounds="true"
+            android:cropToPadding="true"
+            android:src="@drawable/ic_add_event"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"/>
+
+    </LinearLayout>
+    <RelativeLayout
+        android:layout_width="40dp"
+        android:layout_height="40dp"/>
+    <!--Card menu-->
+    <LinearLayout
+        android:layout_weight="1"
+        android:foreground="?android:attr/selectableItemBackground"
+        android:clickable="true"
+        android:background="@drawable/card_button"
+        android:padding="20dp"
+        android:orientation="horizontal"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content">
+        <ImageView
+            android:adjustViewBounds="true"
+            android:cropToPadding="true"
+            android:src="@drawable/ic_add_event"
+            android:layout_width="match_parent"
+            android:layout_height="wrap_content"/>
+
+    </LinearLayout>
+
+
+
+</LinearLayout>
+
+    <TextView
+        android:layout_marginTop="10dp"
+        android:layout_width="match_parent"
+        android:layout_height="wrap_content"
+        android:text="Kategori"
+        android:textAlignment="center"
+        android:textSize="12dp">
+
+    </TextView>
+
 
 </LinearLayout>
